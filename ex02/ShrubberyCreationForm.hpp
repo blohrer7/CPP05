5c6e25ff686ef7b5cee53896d2bab9062ff89c27
Benjamin Lohrer <blohrer@3-A-4.42heilbronn.de>
2026-03-09 09:25:05 +0100

    add Shrubbery

--- ex02/ShrubberyCreationForm.hpp
+++ ex02/ShrubberyCreationForm.hpp
@@ -6,7 +6,29 @@
 /*   By: blohrer <blohrer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 15:13:23 by blohrer           #+#    #+#             */
-/*   Updated: 2026/01/28 15:13:24 by blohrer          ###   ########.fr       */
+/*   Updated: 2026/03/09 08:54:42 by blohrer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SHRUBBERYCREATIONFORM_HPP
+#define SHRUBBERYCREATIONFORM_HPP
+
+#include "AForm.hpp"
+#include <string>
+
+class ShrubberyCreationForm : public AForm
+{
+	private:
+		std::string target;
+
+	public:
+		ShrubberyCreationForm(const std::string& target);
+		ShrubberyCreationForm(const ShrubberyCreationForm& other);
+		ShrubberyCreationForm& operator=(const ShrubberyCreationForm& other);
+		~ShrubberyCreationForm();
+
+	protected:
+		void executeAction() const;
+};
+
+#endif
